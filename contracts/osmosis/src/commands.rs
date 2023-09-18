3b355067fc941515ce6e039dcf0c5463ddc6ace9
nnoln <mark3dom@mail.ru>
2023-09-18 20:26:32 -0300

    osmosis recovery

--- contracts/osmosis/src/commands.rs
+++ contracts/osmosis/src/commands.rs
@@ -1,5 +1,5 @@
 use cosmwasm_std::{
-    to_binary, BankMsg, DepsMut, Env, MessageInfo, Reply, Response, SubMsg, WasmMsg,
+    to_binary, BankMsg, DepsMut, Env, MessageInfo, Reply, Response, StdError, SubMsg, WasmMsg,
 };
 use cw_utils::one_coin;
 use ibc_tracking::{
@@ -16,8 +16,8 @@ use crate::{
     msg::{AfterSwapAction, ExecuteMsg, MsgReplyId, MultiSwapMsg},
     state::{
         load_multi_swap_state, load_swap_reply_state, remove_multi_swap_state,
-        store_multi_swap_state, store_swap_reply_state, swap_reply_state_exists, MultiSwapState,
-        SwapReplyState,
+        remove_swap_reply_state, store_multi_swap_state, store_swap_reply_state,
+        swap_reply_state_exists, MultiSwapState, SwapReplyState,
     },
     ContractError,
 };
@@ -25,7 +25,7 @@ use crate::{
 const TRANSFER_PORT: &str = "transfer";
 const IBC_PACKET_LIFITIME: u64 = 3600u64; // 1 Hour
 
-pub fn swap(
+pub fn handle_swap_with_action(
     deps: DepsMut,
     env: &Env,
     info: &MessageInfo,
@@ -40,9 +40,6 @@ pub fn swap(
         });
     }
 
-    let input_coin = one_coin(info)?;
-    let swap_msg = build_swap_msg(deps.storage, env, input_coin, swap_msg)?;
-
     store_swap_reply_state(
         deps.storage,
         &SwapReplyState {
@@ -51,6 +48,27 @@ pub fn swap(
         },
     )?;
 
+    Ok(Response::new().add_submessage(SubMsg::reply_always(
+        WasmMsg::Execute {
+            contract_addr: env.contract.address.to_string(),
+            msg: to_binary(&ExecuteMsg::ProcessSwap { swap_msg })?,
+            funds: info.funds.clone(),
+        },
+        MsgReplyId::SwapWithActionFallback.repr(),
+    )))
+}
+
+pub fn handle_process_swap(
+    deps: DepsMut,
+    env: &Env,
+    info: &MessageInfo,
+    swap_msg: OsmosisSwapMsg,
+) -> Result<Response, ContractError> {
+    if info.sender.ne(&env.contract.address) {
+        return Err(ContractError::Unauthorized {});
+    }
+
+    let swap_msg = build_swap_msg(deps.storage, env, one_coin(info)?, swap_msg)?;
     Ok(Response::new().add_submessage(SubMsg::reply_on_success(swap_msg, MsgReplyId::Swap.repr())))
 }
 
@@ -143,25 +161,50 @@ pub fn handle_multiswap(
         },
     )?;
 
-    // handle first swap and initiate callback loop
-    handle_multiswap_reply(deps, env)
+    Ok(Response::new().add_submessage(SubMsg::reply_always(
+        WasmMsg::Execute {
+            contract_addr: env.contract.address.to_string(),
+            msg: to_binary(&ExecuteMsg::ProcessMultiSwap {})?,
+            funds: vec![],
+        },
+        MsgReplyId::MultiSwapFallback.repr(),
+    )))
 }
 
-pub fn handle_multiswap_reply(deps: DepsMut, env: &Env) -> Result<Response, ContractError> {
+pub fn handle_multiswap_reply(
+    deps: DepsMut,
+    env: &Env,
+    info: Option<&MessageInfo>,
+) -> Result<Response, ContractError> {
+    if let Some(info) = info {
+        if info.sender.ne(&env.contract.address) {
+            return Err(ContractError::Unauthorized {});
+        }
+    }
+
+    // clean previous swap info
+    remove_swap_reply_state(deps.storage);
+
     let mut multi_swaps = load_multi_swap_state(deps.storage)?;
     if multi_swaps.swaps.is_empty() {
-        // all swaps are done, remove state and return ok
-        remove_multi_swap_state(deps.storage);
+        // all swaps are done, return ok
         return Ok(Response::new());
     }
 
+    // store next swap info
     let next_swap = multi_swaps.swaps.pop().unwrap();
-    let swap_msg = WasmMsg::Execute {
-        contract_addr: env.contract.address.to_string(),
-        msg: to_binary(&ExecuteMsg::SwapWithAction {
-            swap_msg: next_swap.swap_msg,
+    store_swap_reply_state(
+        deps.storage,
+        &SwapReplyState {
             after_swap_action: next_swap.after_swap_action,
             local_fallback_address: multi_swaps.local_fallback_address.clone(),
+        },
+    )?;
+
+    let swap_msg = WasmMsg::Execute {
+        contract_addr: env.contract.address.to_string(),
+        msg: to_binary(&ExecuteMsg::ProcessSwap {
+            swap_msg: next_swap.swap_msg,
         })?,
         funds: vec![next_swap.amount_in],
     };
@@ -171,4 +214,57 @@ pub fn handle_multiswap_reply(deps: DepsMut, env: &Env) -> Result<Response, Cont
         swap_msg,
         MsgReplyId::MultiSwap.repr(),
     )))
+}
+
+pub fn handle_swap_with_action_fallback_reply(
+    deps: DepsMut,
+    env: &Env,
+    reply: Reply,
+) -> Result<Response, ContractError> {
+    let swap_state = load_swap_reply_state(deps.storage)?;
+    remove_swap_reply_state(deps.storage);
+
+    // in case of no error finish execution
+    if reply.result.is_ok() {
+        return Ok(Response::new());
+    }
+
+    // otherwise transfer all owned funds to the fallback address
+    recover_funds(deps, env, &swap_state.local_fallback_address)
+}
+
+pub fn handle_multiswap_fallback_reply(
+    deps: DepsMut,
+    env: &Env,
+    reply: Reply,
+) -> Result<Response, ContractError> {
+    let multi_swaps = load_multi_swap_state(deps.storage)?;
+    remove_multi_swap_state(deps.storage);
+
+    // in case of no error finish execution
+    if reply.result.is_ok() {
+        return Ok(Response::new());
+    }
+
+    // otherwise transfer all owned funds to the fallback address
+    recover_funds(deps, env, &multi_swaps.local_fallback_address)
+}
+
+fn recover_funds(
+    deps: DepsMut,
+    env: &Env,
+    fallback_address: &str,
+) -> Result<Response, ContractError> {
+    let owned_coins = deps
+        .querier
+        .query_all_balances(env.contract.address.to_string())?;
+
+    if owned_coins.is_empty() {
+        return Err(StdError::generic_err("Nothing to recover, forwarding error").into());
+    }
+
+    Ok(Response::new().add_message(BankMsg::Send {
+        to_address: fallback_address.to_owned(),
+        amount: owned_coins,
+    }))
 }
